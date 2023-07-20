e5d913cefb67c7bb2026ddaeb106ade6a802217b
Muhammed Yasin <mohamedyasin2161@gmail.com>
2023-07-20 15:11:48 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      display: ["Poppins"],
+    },
+    extend: {},
+  },
+  plugins: [],
+  zIndex: {
+    100: "100",
+  },
+};
